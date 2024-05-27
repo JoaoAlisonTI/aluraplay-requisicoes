ec2ee91b5cd0cb574a4a40b3661eec4ac2abc2c2
JoaoAlisonTI <joaoalison.ti@gmail.com>
2024-05-27 17:02:13 -0300

    feat: Aula 02 - Adicionando função de listagem dos videos

---
+++ js/conectaApi.js
@@ -0,0 +1,10 @@
+async function buscarVideos() {
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
+export const conectaApi = {
+    buscarVideos
+}
---
+++ js/mostrarVideos.js
@@ -0,0 +1,27 @@
+import { conectaApi } from "./conectaApi.js";
+
+const lista = document.querySelector("[data-lista]");
+
+function constroiCard(titulo, descricao, url, imagem) {
+    const video = document.createElement("li");
+    video.className = "videos__item";
+    video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
+    title="${titulo}" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
+<div class="descricao-video">
+    <img src="${imagem}" alt="logo canal alura">
+    <h3>${titulo}</h3>
+    <p>${descricao}</p>
+</div>`
+
+    return video;
+}
+
+async function listaVideos() {
+    const listaApi = await conectaApi.buscarVideos();
+    listaApi.forEach(elemento => lista.appendChild(
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+}
+
+listaVideos();
